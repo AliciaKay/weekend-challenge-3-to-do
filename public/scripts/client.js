f61eb7c89546de759045fd0df7083ea68a873cfe
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-17 11:49:00 -0500

    debugging in progress for favicon and confirmation pop-up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,13 @@ $(document).ready(onReady);
 function onReady(){
     getTask();
     $('#addButton').on('click', addTask);
-    $('#allTasks').on('click', '.deleteMe', deleteTask);
-    $('#completedTable').on('click', '.deleteMe', deleteTask);
-    $('#allTasks').on('change', '.checked', completeTask);
+    $('[data-toggle=confirmation]').confirmation({
+        rootSelector: '[data-toggle=confirmation]',
+        // other options
+      });
+      $('#allTasks').on('click', '.deleteMe', deleteTask);
+      $('#completedTable').on('click', '.deleteMe', deleteTask);
+      $('#allTasks').on('change', '.checked', completeTask);
 };
 
 function getTask(){
@@ -32,7 +36,7 @@ function getTask(){
                     } else if (completeStatus === true) {
                         $row.append('<td>' + '<input class="checked" type="checkbox" data-id="' + taskId + '"checked></td>');
                     } 
-                var $deleteButton = $('<td><button class="deleteMe btn-danger" data-id="' + taskId + '">Remove</button></td>');
+                var $deleteButton = $('<td><button class="btn btn-danger" data-toggle="confirmation" data-btn-ok-label="Continue" data-btn-ok-icon="glyphicon glyphicon-share-alt" data-btn-ok-class="btn-success" data-btn-cancel-label="Cancel" data-btn-cancel-icon="glyphicon glyphicon-ban-circle" data-btn-cancel-class="btn-danger" data-title="Remove from task history?" data-content="This action cannot be undone" data-id="' + taskId + '">Remove</button></td>');
                 $row.append($deleteButton);
                 $('#allTasks').append($row); 
                 appendCompletedTask();
@@ -64,16 +68,17 @@ function addTask(){
 function deleteTask(){
     console.log('inside deleteTask row 56', thisId);
     var thisId = $(this).data('id');
-    if (confirm('Are you sure you want to remove this task? (This action cannot be undone)'))
+    if (confirmed.bs.confirmation) {
     $.ajax({
         method: 'DELETE',
         url: '/tasks/' + thisId,
         success: function(response){
             console.log('ajax post task', response);
             getTask();
-        }
-    })
-};
+            }
+        })
+    };
+}
 
 function completeTask() {
     var thisId = $(this).data('id');
