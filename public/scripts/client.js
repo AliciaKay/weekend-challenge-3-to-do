79a8ee26d3017f86c7fc224293f040ea892dccf1
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-18 07:42:04 -0500

    added comments to clarify

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,29 @@
 $(document).ready(onReady);
 
 function onReady(){
+    //get complete list
     getTask();
+    //when the add button is clicked, add a task
     $('#addButton').on('click', addTask);
+    //if a button of the class deleteMe is clicked on the allTasks table, run delete task function
     $('#allTasks').on('click', '.deleteMe', deleteTask);
+    //if a button of the class deleteMe is clicked on the completedTable table, run delete task function
     $('#completedTable').on('click', '.deleteMe', deleteTask);
+    //if the state of the checkbox of the class checked is clicked on the allTasks table, run the completeTask function
     $('#allTasks').on('change', '.checked', completeTask);
 };
 
 function getTask(){
-  
+    //ajax GET info from the server
     $.ajax({
         method: 'GET',
         url: '/tasks',
         success: function(response){
             console.log('ajax get task', response);
+            //empty both tables
             $('#allTasks').empty();
-            $('#completedTable').empty();  
+            $('#completedTable').empty(); 
+            //loop through the server response array and assign variables 
             for(var i = 0; i < response.length; i++){
                 var task = response[i];
                 var taskId = task.id;
@@ -24,9 +31,11 @@ function getTask(){
                 var taskDesc = task.description;
                 var completeStatus = task.complete;
                 console.log('line 19 of GET TASK', task);
+                //append each item to the DOM as a table row with a checkbox and a delete button
                 var $row = $('<tr class="table-striped"></tr>');
                 $row.append('<td>' + taskName + '</td>');
                 $row.append('<td>' + taskDesc + '</td>');
+                //if the task is recorded in the DB as complete, check the checkbox
                     if (completeStatus === false) {
                         $row.append('<td>' + '<input class="checked" type="checkbox" data-id="' + taskId+ '"></td>');
                     } else if (completeStatus === true) {
@@ -34,7 +43,8 @@ function getTask(){
                     } 
                 var $deleteButton = $('<td><button class="deleteMe btn-danger" data-id="' + taskId + '">Remove</button></td>');
                 $row.append($deleteButton);
-                $('#allTasks').append($row); 
+                $('#allTasks').append($row);
+                //run the append completed tasks on all items that have checked boxes 
                 appendCompletedTask();
                 };
         }
@@ -42,20 +52,24 @@ function getTask(){
 };
 
 function addTask(){
+    //take values from the input boxes and create an object
     var objectToSend ={
         name: $('#nameIn').val(),
         description: $('#taskIn').val(),
+        //initally mark each task as incomplete
         complete: false
     }
+    //empty out the input boxes
     $('#nameIn').val(''); 
     $('#taskIn').val('');
-
+    //ajax POST the object created from the values to the server
     $.ajax({
         method: 'POST',
         url: '/tasks',
         data: objectToSend,
         success: function(response){
             console.log('ajax post task', response);
+            //run the getTask function to refresh the DOM
             getTask();
         }
     })
@@ -63,8 +77,10 @@ function addTask(){
 
 function deleteTask(){
     console.log('inside deleteTask row 56', thisId);
-    var thisId = $(this).data('id');            
-    BootstrapDialog.confirm({
+    //assign a variable based on the object's id
+    var thisId = $(this).data('id');   
+    //create a confirm dialog box with custom text and buttons (bootstrap + additional plugin)     
+      BootstrapDialog.confirm({
         title: 'Delete Task From History?',
         message: 'This action cannot be undone.',
         type: BootstrapDialog.TYPE_WARNING, 
@@ -75,12 +91,14 @@ function deleteTask(){
         btnOKLabel: 'Delete', 
         btnOKClass: 'btn-danger',
         callback: function(result) {
+            //if the user confirms, ajax DELETE the item from the DB that matches the id in the variable
             if(result) {
                 $.ajax({
                     method: 'DELETE',
                     url: '/tasks/' + thisId,
                     success: function(response){
                         console.log('ajax post task', response);
+                        //run the getTask function again to refresh the DOM
                         getTask();
                     }
                 })
@@ -88,29 +106,39 @@ function deleteTask(){
         }
     });
 }
-
+//when the checkbox is checked...
 function completeTask() {
+    //assign variable based on this object's id
     var thisId = $(this).data('id');
+    //wrap it in an object to send to DB
     var taskToSend = {
         id: thisId,
     };
+    //ajax PUT the updated object that matches the id in the variable
     $.ajax({
         method: 'PUT',
         url: '/tasks/' + thisId,
         data: taskToSend,
         success: function(response){
             console.log('ajax put task', response);
+            //run the append function 
             appendCompletedTask();
         }
     })
 };
 
+
+
+//for each checked box in the table, append its row to the completedTable table
 function appendCompletedTask() {
     $('table tbody').find('input[class="checked"]').each(function() {
         if($(this).is(':checked')){ 
             $('#completedTable').append($(this).parents('tr'));
+            //add the success class (bootstrap class) to change the appearance of the row
             $(this).parents().addClass('success');
+            //replace the checked box with a static glyphicon
             $(this).replaceWith('<span class="glyphicon glyphicon-ok-sign"></span>');
+            //remove the original row from the all tasks table
             $(this).parents("tr").remove();
         }
     })
--- routers/router.js
+++ routers/router.js
@@ -1,8 +1,12 @@
+//require the custom router file
 var router = require('express').Router();
+//require the pool file
 var pool = require('../modules/pool');
 
+//when the get call is made...
 router.get('/', function (req, res) {
     console.log('in the tasks get');
+    //connect the the pool file and tell the client to send all table items back
     pool.connect(function (conErr, client, done){
         if (conErr){
             console.log(conErr);
@@ -21,9 +25,11 @@ router.get('/', function (req, res) {
         }
     })
 });
-
+//when the post call is made...
 router.post('/', function (req, res) {
+    //assign a variable to hold the item sent from the front-end client, req.body
     var newTask = req.body;
+    //connect the pool file and tell the client to insert the posted item to the DB, using the variable and the object's properties
     pool.connect(function (conErr, client, done){
         if (conErr){
             res.sendStatus(500);
@@ -39,9 +45,11 @@ router.post('/', function (req, res) {
         }
     })
 });
-
+//when the delete call is made...
 router.delete('/:id', function(req,res){
+    //assign a variable to hold the id of the item being deleted, req.params.id
     var dbId= req.params.id;
+    //connect the pool file and tell the client to delete the item that matches the id within the variable
     pool.connect(function (conErr, client, done){
         if (conErr){
             console.log(conErr);
@@ -58,9 +66,11 @@ router.delete('/:id', function(req,res){
         }
     }
 )}); 
-
+//when the put call is made...
 router.put('/:id', function(req, res) {
+    //assign a variable to hold the id of the item being updated, req.params.id
     var taskCompleteId = req.params.id;
+    //connect to the pool file and tell the client to update the item that matches the id within the variable 
     pool.connect(function(connectError, client, done) {
         if (connectError) {
             console.log('connection error:', connectError);
@@ -79,5 +89,5 @@ router.put('/:id', function(req, res) {
         }
     });
 });
-
+//export this router as a module
 module.exports = router;
